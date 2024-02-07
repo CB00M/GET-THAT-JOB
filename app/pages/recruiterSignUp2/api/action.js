c1ac439b1230d0eb37b2273c1ef706adc6e9273a
Jiran <paiveryhappy@gmail.com>
2024-02-07 13:07:24 +0700

    feat: recruiter and user can send data to database

--- app/pages/recruiterSignUp2/api/action.js
+++ app/pages/recruiterSignUp2/api/action.js
@@ -2,17 +2,22 @@
 import { createClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 import { supabaseUrl, supabaseKey } from "../../../../utils/supabase/supabase";
+
 import { v4 as uuidv4 } from "uuid";
 
 async function signUp(formData) {
   const cookieStore = cookies();
   const supabase = createClient(supabaseUrl, supabaseKey);
-  const companyWebsite = formData.get("companyWebsite");
+
+  const company = formData.get("company-name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const website = formData.get("website");
   const aboutCompany = formData.get("aboutCompany");
   const attachment = formData.get("attachment");
   const fileName = uuidv4();
 
-  console.log("attachment:", attachment);
+  // console.log("attachment:", attachment);
 
   const { error } = await supabase.storage
     .from("attachments")
@@ -25,22 +30,24 @@ async function signUp(formData) {
     .from("attachments")
     .getPublicUrl(fileName);
 
-  console.log("Upload Successful:", publicAttachmentUrl);
-  const { data, insertError } = await supabase
-    .from("recruiterInformation")
-    .insert([
-      {
-        company_website: companyWebsite,
-        about_company: aboutCompany,
-        attachment: publicAttachmentUrl.data.publicUrl,
-      },
-    ]);
+  console.log("Upload Company Logo Successful:", publicAttachmentUrl);
+  const { data, insertError } = await supabase.from("Recruiterusers").insert([
+    {
+      company,
+      email,
+      password,
+      website,
+      about_company: aboutCompany,
+      attachment: publicAttachmentUrl.data.publicUrl,
+    },
+  ]);
 
   if (insertError) {
     console.error("found some error", insertError);
     return false;
   }
-  console.log("register successful");
+  console.log("register successfully !!");
+  console.log(company, email, password, website, aboutCompany);
 }
 
 export default signUp;
