058abac7466c646d6f7faa9f970fbb55bacc5e0c
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2024-02-08 12:25:40 +0700

    fix:form submit on recruiter page

--- app/pages/recruiterSignUp2/api/recruiterRegister.js
+++ app/pages/recruiterSignUp2/api/recruiterRegister.js
@@ -1,16 +1,33 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import { v4 as uuidv4 } from "uuid";
 
 export async function register(formData) {
   const cookiesStore = cookies();
   const supabase = createClient(cookiesStore);
 
-  const company = formData.get("company");
+  const company = formData.get("company-name");
   const email = formData.get("email");
   const password = formData.get("password");
   const website = formData.get("website");
   const about = formData.get("aboutCompany");
+  const attachment = formData.get("attachment");
+  const fileName = uuidv4();
+  console.log("attachment:", attachment);
+
+  const { errorAttachments } = await supabase.storage
+    .from("attachments")
+    .upload(fileName, attachment);
+  if (errorAttachments) {
+    console.log("error:", error);
+    return false;
+  }
+  const publicAttachmentUrl = supabase.storage
+    .from("attachments")
+    .getPublicUrl(fileName);
+
+  console.log("Upload Successful:", publicAttachmentUrl);
 
   const { dataAuth, errorAuth } = await supabase.auth.signUp({
     email,
