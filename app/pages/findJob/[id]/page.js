6614e61971688a8b124f053feb766e988b5ee9b8
Jiran <paiveryhappy@gmail.com>
2024-02-14 23:31:30 +0700

    feat: add function dynamic router

--- app/pages/findJob/[id]/page.js
+++ app/pages/findJob/[id]/page.js
@@ -1,16 +1,35 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Montserrat, Inter } from "next/font/google";
 import "tailwindcss/tailwind.css";
 import Sidebar from "@/app/components/ProfessionalSidebar/page";
 import { HiChevronLeft } from "react-icons/hi";
 import { MdOutlineWatchLater } from "react-icons/md";
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({ weight: "400", preload: false });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export default function page() {
+export default function post({ params }) {
+  const router = useRouter();
+  const supabase = createClient();
+  const [job, setJob] = useState([]);
+  const getDetailJob = async () => {
+    let { data, error } = await supabase
+      .from("job_posting")
+      .select("*")
+      .eq("id", params.id);
+    if (error || !data) {
+      console.log("error:", error);
+    }
+    setJob(data[0]);
+  };
+  useEffect(() => {
+    getDetailJob();
+  }, [params.id]);
+
   return (
     <>
       <div className="w-full h-[1050px] bg-neutral-100  items-start inline-flex">
@@ -20,7 +39,14 @@ export default function page() {
         <div className="w-[1200px] h-[800px] py-[10px] px-[20px] ml-[150px]">
           <div className=" flex">
             <HiChevronLeft className="w-6 h-5 items-center" />
-            <p className="text-[14px]">BACK</p>
+            <button
+              onClick={() => {
+                router.push("/pages/findJob");
+              }}
+              className="text-[14px]"
+            >
+              BACK
+            </button>
           </div>
           <article className="mt-5">
             <header className="flex justify-between items-center">
@@ -52,7 +78,7 @@ export default function page() {
                 </button>
               </div>
             </header>
-            <h1 className="text-center text-5xl">The job title</h1>
+            <h1 className="text-center text-5xl">{job.title}</h1>
             <p className="flex items-center gap-1 text-[10px] justify-center mt-3">
               <MdOutlineWatchLater />
               POSTED 2 DAYS AGO
@@ -66,7 +92,7 @@ export default function page() {
                     width={29}
                     height={29}
                   ></Image>
-                  Manufacturing
+                  {job.category}
                 </p>
               </div>
               <div className="h-[77px] w-1/5 border border-[#BF5F82] flex flex-col justify-center items-center rounded-lg  p-2 bg-white">
@@ -77,7 +103,7 @@ export default function page() {
                     width={29}
                     height={29}
                   ></Image>
-                  Full time
+                  {job.type}
                 </p>
               </div>
               <div className="h-[77px] w-2/6 border border-[#BF5F82] flex flex-col justify-center items-center rounded-lg  p-2 bg-white">
@@ -88,14 +114,14 @@ export default function page() {
                     width={29}
                     height={29}
                   ></Image>
-                  2000 - 2500
+                  {job.minRange} - {job.maxRange}
                 </p>
               </div>
             </div>
             <div className="flex flex-col">
               <div>
                 <h3 className="text-2xl text-[#BF5F82] my-2">
-                  About The company name SA
+                  About The company
                 </h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
@@ -111,43 +137,19 @@ export default function page() {
                 <h3 className="text-2xl text-[#BF5F82] my-2">
                   About the job position
                 </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                  blanditiis praesentium eius doloremque, iusto ratione in
-                  aspernatur vitae saepe quis architecto consequatur accusamus
-                  ad iure esse, optio repudiandae? Animi doloribus temporibus
-                  esse vitae praesentium maxime molestias. Consequatur, saepe.
-                  Quis impedit ut, dolores adipisci facere deserunt libero
-                  deleniti similique nobis sequi.
-                </p>
+                <p>{job.aboutJob}</p>
               </div>
               <div className="my-2">
                 <h3 className="text-2xl text-[#BF5F82] my-2">
                   Mandotory Requirments
                 </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                  blanditiis praesentium eius doloremque, iusto ratione in
-                  aspernatur vitae saepe quis architecto consequatur accusamus
-                  ad iure esse, optio repudiandae? Animi doloribus temporibus
-                  esse vitae praesentium maxime molestias. Consequatur, saepe.
-                  Quis impedit ut, dolores adipisci facere deserunt libero
-                  deleniti similique nobis sequi.
-                </p>
+                <p>{job.mandaturyRequier}</p>
               </div>
               <div className="my-2">
                 <h3 className="text-2xl text-[#BF5F82] my-2">
                   Optional Requirments
                 </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                  blanditiis praesentium eius doloremque, iusto ratione in
-                  aspernatur vitae saepe quis architecto consequatur accusamus
-                  ad iure esse, optio repudiandae? Animi doloribus temporibus
-                  esse vitae praesentium maxime molestias. Consequatur, saepe.
-                  Quis impedit ut, dolores adipisci facere deserunt libero
-                  deleniti similique nobis sequi.
-                </p>
+                <p>{job.optionalRequier}</p>
               </div>
 
               <button className="mt-3 m-auto">
