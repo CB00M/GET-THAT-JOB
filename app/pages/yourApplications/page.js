36a87a8d09449b26be3422bcc3ca869ca902ef4f
du <tb.poopp@gmail.com>
2024-02-19 16:08:30 +0700

    feat: add fetching and map from data

--- app/pages/yourApplications/page.js
+++ app/pages/yourApplications/page.js
@@ -2,7 +2,8 @@
 import "tailwindcss/tailwind.css";
 import Image from "next/image";
 import { Montserrat, Inter } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 import { Cookie } from "next/font/google";
 import { keepData } from "../createNewJobPosting/api/createNewPost.js";
 import Link from "next/link.js";
@@ -20,6 +21,7 @@ const inter = Inter({ weight: "400", preload: false });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function page() {
+  const supabase = createClient();
   const [title, setTitle] = useState("");
   //category
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -31,6 +33,27 @@ export default function page() {
   const [aboutJob, setAboutJob] = useState("");
   const [mandaturyRequier, setMandaturyRequier] = useState("");
   const [optionalRequier, setOptionalRequier] = useState("");
+  const [jobs, setJobs] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("your_applications")
+        .select("*");
+
+      if (error) {
+        console.error("Error fetching jobs:", error.message);
+      } else {
+        setJobs(data || []);
+      }
+    } catch (error) {
+      console.error("Error fetching jobs:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const handleCategoryChange = (event) => {
     const value = event.target.value;
@@ -61,6 +84,7 @@ export default function page() {
     setMandaturyRequier(mandaturyRequier);
     setOptionalRequier(optionalRequier);
   };
+
   return (
     <>
       <div className="w-full h-[900px] bg-neutral-100  items-start inline-flex">
@@ -80,7 +104,7 @@ export default function page() {
               className="text-zinc-600 text-[10px] uppercase relative top-[3px] tracking-[.10em] "
               style={inter.style}
             >
-              filter your applications
+              4 filter your applications
             </div>
             <div
               className="pt-2 inline-flex gap-4 text-zinc-600 text-sm leading-tight tracking-[.01em] "
@@ -144,144 +168,153 @@ export default function page() {
               className="  text-neutral-700 text-[22px] font-medium text-left my-[10px]"
               style={montserrat.style}
             >
-              4 applications found
+              {jobs.length} applications found
             </h2>
           </div>
           {/* <----------------------------------------------------candidate boxs--------------------------------------------------> */}
           <div>
             <Accordion allowToggle>
               {/*Job title*/}
-              <div className="border border-slate-300 shadow-lg shadow-slate-400  rounded-[10px] w-[1080px]  p-5">
-                <AccordionItem w="1050px">
-                  <h2>
-                    <div className="warpper flex relative">
-                      <AccordionButton>
-                        <span className="flex flex-row w-[1020px] h-[70px] mb-3">
-                          <div className=" w-[350px] p-2 flex flex-row">
-                            {/*left-container*/}
-                            <Image src="/logo.png" width={60} height={60} />
-
-                            <div className="flex flex-col ml-4">
-                              <h2
-                                className="   text-[22px] font-medium text-left"
-                                style={montserrat.style}
-                              >
-                                The job title
-                              </h2>
-
-                              <p className="  text-[#616161] text-[16px] font-medium">
-                                The Company Name SA
-                              </p>
-                            </div>
-                          </div>
-                          <div className="flex flex-row  ">
-                            {/* middle-container */}
-                            <div className="text-[#8e8e8e] text-[12px] mr-[150px]">
-                              <div className="flex flex-row justify-start content-end  ">
-                                <div className="flex flex-row ml-1 ">
-                                  <Image
-                                    src="/factory.svg"
-                                    width={15}
-                                    height={20}
-                                  />
-                                  <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
-                                    Manufactoring
-                                  </p>
-                                </div>
-                                <div className="flex flex-row ml-1">
-                                  <Image
-                                    src="/calendar-2-line.svg"
-                                    width={15}
-                                    height={20}
-                                  />
-                                  <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
-                                    Full time
-                                  </p>
-                                </div>
-                              </div>
-                              <div className="flex flex-row   relative bottom-3">
-                                <div className="flex flex-row ml-1 ">
-                                  <Image
-                                    src="/dollar.svg"
-                                    width={15}
-                                    height={20}
-                                  />
-                                  <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
-                                    2.0k - 2.5k
-                                  </p>
-                                </div>
-                                <div className="flex flex-row mr-1 ">
-                                  <Image
-                                    src="/clock.svg"
-                                    width={15}
-                                    height={20}
-                                  />
-                                  <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
-                                    Posted 2 days ago
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="flex flex-col justify-center items-center m-2 ">
-                              <Image
-                                src="/mail-line-black.svg"
-                                width={20}
-                                height={20}
-                              />
-                              <p>Sent 5 days</p>
-                              <p>ago</p>
-                            </div>
-                            <div className="flex flex-col justify-center items-center m-2">
-                              <Image
-                                src="/pause-icon.svg"
-                                width={20}
-                                height={20}
-                              />
-                              <p className="text-[#f495b5]">Waiting for</p>
-                              <p className="text-[#f495b5]">review</p>
-                            </div>
-                          </div>
-                        </span>
-                        <AccordionIcon />
-                      </AccordionButton>
-                    </div>
-                  </h2>
-                  <AccordionPanel
-                    pb={4}
-                    style={montserrat.style}
-                    className="font-medium"
+              {jobs.map((job) => {
+                return (
+                  <div
+                    className="border border-slate-300 shadow-lg shadow-slate-400  rounded-[10px] w-[1080px]  p-5"
+                    key={job.id}
                   >
-                    {" "}
-                    <h2 className="  text-[#c67190] text-[20px] font-medium text-left my-2">
-                      Professional experience
-                    </h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <h2 className="  text-[#c67190] text-[20px] font-medium text-left my-2">
-                      Why are you interested in working at the company name SA
-                    </h2>
-                    <p>-Lorem ipsum dolor sit amet, consectetur</p>
-                    <p>-Lorem ipsum dolor sit amet, consectetur</p>
-                    <p>-Lorem ipsum dolor sit amet, consectetur</p>
-                    <p>-Lorem ipsum dolor sit amet, consectetur</p>
-                    <div className="flex relative left-[400px] bottom-[5px] mt-[20px]">
-                      <Image
-                        src="/x-icon.svg"
-                        width={28}
-                        height={20}
-                        className="absolute left-[14px] top-[10px]"
-                      />
-                      <button className=" w-[260px] h-[50px] bg-[#bf5f82] text-white rounded-[22px] text-[16px] text-right pr-[20px]  uppercase ">
-                        decline application
-                      </button>
-                    </div>
-                  </AccordionPanel>
-                </AccordionItem>
-              </div>
+                    <AccordionItem w="1050px">
+                      <h2>
+                        <div className="warpper flex relative">
+                          <AccordionButton>
+                            <span className="flex flex-row w-[1020px] h-[70px] mb-3">
+                              <div className=" w-[350px] p-2 flex flex-row">
+                                {/*left-container*/}
+                                <Image src="/logo.png" width={60} height={60} />
+
+                                <div className="flex flex-col ml-4">
+                                  <h2
+                                    className="   text-[22px] font-medium text-left"
+                                    style={montserrat.style}
+                                  >
+                                    {job.title}
+                                  </h2>
+
+                                  <p className="  text-[#616161] text-[16px] font-medium">
+                                    The Company Name SA
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="flex flex-row  ">
+                                {/* middle-container */}
+                                <div className="text-[#8e8e8e] text-[12px] mr-[150px]">
+                                  <div className="flex flex-row justify-start content-end  ">
+                                    <div className="flex flex-row ml-1 ">
+                                      <Image
+                                        src="/factory.svg"
+                                        width={15}
+                                        height={20}
+                                      />
+                                      <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
+                                        Manufactoring
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-row ml-1">
+                                      <Image
+                                        src="/calendar-2-line.svg"
+                                        width={15}
+                                        height={20}
+                                      />
+                                      <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
+                                        Full time
+                                      </p>
+                                    </div>
+                                  </div>
+                                  <div className="flex flex-row   relative bottom-3">
+                                    <div className="flex flex-row ml-1 ">
+                                      <Image
+                                        src="/dollar.svg"
+                                        width={15}
+                                        height={20}
+                                      />
+                                      <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
+                                        2.0k - 2.5k
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-row mr-1 ">
+                                      <Image
+                                        src="/clock.svg"
+                                        width={15}
+                                        height={20}
+                                      />
+                                      <p className=" py-[10px] text-[#8e8e8e] text-[16px] mx-2">
+                                        Posted 2 days ago
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="flex flex-col justify-center items-center m-2 ">
+                                  <Image
+                                    src="/mail-line-black.svg"
+                                    width={20}
+                                    height={20}
+                                  />
+                                  <p>Sent 5 days</p>
+                                  <p>ago</p>
+                                </div>
+                                <div className="flex flex-col justify-center items-center m-2">
+                                  <Image
+                                    src="/pause-icon.svg"
+                                    width={20}
+                                    height={20}
+                                  />
+                                  <p className="text-[#f495b5]">Waiting for</p>
+                                  <p className="text-[#f495b5]">review</p>
+                                </div>
+                              </div>
+                            </span>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </div>
+                      </h2>
+                      <AccordionPanel
+                        pb={4}
+                        style={montserrat.style}
+                        className="font-medium"
+                      >
+                        {" "}
+                        <h2 className="  text-[#c67190] text-[20px] font-medium text-left my-2">
+                          Professional experience
+                        </h2>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat.
+                        </p>
+                        <h2 className="  text-[#c67190] text-[20px] font-medium text-left my-2">
+                          Why are you interested in working at the company name
+                          SA
+                        </h2>
+                        <p>-Lorem ipsum dolor sit amet, consectetur</p>
+                        <p>-Lorem ipsum dolor sit amet, consectetur</p>
+                        <p>-Lorem ipsum dolor sit amet, consectetur</p>
+                        <p>-Lorem ipsum dolor sit amet, consectetur</p>
+                        <div className="flex relative left-[400px] bottom-[5px] mt-[20px]">
+                          <Image
+                            src="/x-icon.svg"
+                            width={28}
+                            height={20}
+                            className="absolute left-[14px] top-[10px]"
+                          />
+                          <button className=" w-[260px] h-[50px] bg-[#bf5f82] text-white rounded-[22px] text-[16px] text-right pr-[20px]  uppercase ">
+                            decline application
+                          </button>
+                        </div>
+                      </AccordionPanel>
+                    </AccordionItem>
+                  </div>
+                );
+              })}
             </Accordion>
           </div>
         </div>
